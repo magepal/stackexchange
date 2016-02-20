ef7189ed00547ad2638847c0a3a1bde78e42f449
R.S <srenon@users.noreply.github.com>
2016-02-20 10:54:04 -0500

    Added files via upload

---
+++ 102629/Gift/Controller/Adminhtml/Gift/Index.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace   MagePal\Gift\Controller\Adminhtml\Gift;
+
+class Index extends \Magento\Backend\App\Action
+{
+    public function execute()
+    {
+        die('dd');
+    }
+}
